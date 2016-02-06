f004d5307e33a5fb5c816d31c6434b467421fc02
Marcel <bernicmarcel@gmail.com>
2016-02-06 16:54:34 -0500

    simple test 2

---
+++ tests/dummyTest2.py
@@ -0,0 +1,14 @@
+#! /bin/python
+
+import unittest
+
+class LearningCase(unittest.TestCase):
+    def test_starting_out(self):
+        self.assertEqual(1, 1)
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
+
