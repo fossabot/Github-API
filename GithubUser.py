55bbb54fa61ca27ba0071c8c8e62121071e0dd8d
Luis E. Obando <luiseduardo.obando@gmail.com>
2016-02-11 17:52:39 -0500

    Add functions to retrieve projects issues

--- GithubUser.py
+++ GithubUser.py
@@ -1,4 +1,5 @@
 from github import Github
+import json
 
 class GithubUser:
     ''' 
@@ -19,7 +20,8 @@ class GithubUser:
                         "languages": repo.get_languages(),
                         "contributors": self.get_contributors(repo),
                         "branches" : self.get_branches(repo),
-                        "open_issues_count" : repo.open_issues_count
+                        "open_issues_count" : repo.open_issues_count,
+                        "open_issues": self.get_all_issues(repo)
                         }
             self.projects.append(project)
 
@@ -48,4 +50,73 @@ class GithubUser:
 
         return branches  
 
+    def get_all_issues(self, repo):
+        issues = []
+        for issue in repo.get_issues():         
+        
+#            test = json.dumps(list(issue.user.__dict__))
+            #print(test)
+            dic_issue = {"id" : issue.id,
+                        "url" : issue.url,
+                        "repository_url" : issue.repository.url,
+                        "labels_url" : issue.labels_url,
+                        "comments_url" : issue.comments_url,
+                        "events_url" : issue.events_url,
+                        "html_url" : issue.html_url,
+                        "number" : issue.number,
+                        "state" : issue.state,
+                        "title" : issue.title,
+                        "body" : issue.body,
+                        "user" : self.get_user_info(issue.user),
+                        "assignee" : self.get_user_info(issue.assignee),
+                        "comments" : issue.comments,
+                        "closed_at" : None if issue.closed_at is None else issue.closed_at.isoformat(),
+                        "created_at": None if issue.created_at is None else issue.created_at.isoformat(),
+                        "updated_at" : None if issue.updated_at is None  else issue.updated_at.isoformat()
+                        }
+            issues.append(dic_issue)
+        return issues
+    
+    def get_user_info(self, user):
+        dic_user_info = {"login" : user.login,
+                         "id": user.id,
+                         "avatar_url": user.avatar_url,
+                         "gravatar_id" : user.gravatar_id,
+                         "url": user.id,
+                         "html_url" : user.html_url,
+                         "followers_url" : user.followers_url,
+                         "gists_url" : user.gists_url,
+                         "starred_url" : user.starred_url,
+                         "subscriptions_url" : user.subscriptions_url,
+                         "organizations_url" : user.organizations_url,
+                         "repos_url" : user.repos_url,
+                         "events_url" : user.events_url,
+                         "received_events_url" : user.received_events_url,
+                         "type" : user.type
+                         }
+                         
+        return dic_user_info
 
+    def get_milestone(self, milestone):
+        milestone_dic = {"url" : milestone.url,
+                         "hmtl_url" : milestone.html_url,
+                         "labels_url" : milestone.labels_url,
+                         "id" : milestone.id,
+                         "number" : milestone.number,
+                         "state" : milestone.state,
+                         "title" : milestone.title,
+                         "description" : milestone.description,
+                         "creator" : self.get_user_info(milestone.creator),
+                         "open_issues" : milestone.open_issues,
+                         "closed_issues" : milestone.closed_issues,
+                         "created_at": milestone.created_at,
+                         "updated_at" : get_isoformat_date(milestone.updated_at),
+                         "closed_at": get_isoformat_date(milestone.closed_at),
+                         "due_on" : get_isoformat_date(milesonte.due_on)
+                         }
+
+    def to_JSON(self, obj):
+        return json.dumps(obj, skipkeys=True, default=lambda o: o.__dict__,sort_keys=True, indent=4)
+
+    def get_isoformat_date(self, date):
+        return None if date is None else date.isoformat()
