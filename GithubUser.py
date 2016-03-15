1cb1b35fef9599d12fcd86065330f657d809ddcd
Luis E. Obando <luiseduardo.obando@gmail.com>
2016-03-15 17:59:51 -0400

    Fix code style

--- GithubUser.py
+++ GithubUser.py
@@ -1,10 +1,9 @@
 from github import Github
-import json
-
 
 class GithubUser:
-    ''' 
-        Class that returns informations about your Github projects 
+
+    '''
+        Class that returns informations about your Github projects
     '''
 
     def __init__(self, token):
@@ -54,27 +53,35 @@ class GithubUser:
     def get_all_issues(self, repo):
         issues = []
         for issue in repo.get_issues():
-            dic_issue = {"id": issue.id,
-                         "url": issue.url,
-                         "repository_url": issue.repository.url,
-                         "labels_url": issue.labels_url,
-                         "comments_url": issue.comments_url,
-                         "events_url": issue.events_url,
-                         "html_url": issue.html_url,
-                         "number": issue.number,
-                         "state": issue.state,
-                         "title": issue.title,
-                         "body": issue.body,
-                         "user": self.get_user_info(issue.user),
-                         "assignee": None if issue.assignee is None else self.get_user_info(issue.assignee),
-                         "milestone": None if issue.milestone is None else self.milestone(issue.milestone),
-                         "labels": None if issue.labels is None else self.get_labels(issue.labels),
-                         "comments": issue.comments,
-                         "pull_request": None if issue.pull_request is None else self.get_pull_request(issue.pull_request),
-                         "closed_at": self.get_isoformat_date(issue.closed_at),
-                         "created_at": self.get_isoformat_date(issue.created_at),
-                         "updated_at": self.get_isoformat_date(issue.updated_at)
-                         }
+            dic_issue = {
+                "id": issue.id,
+                "url": issue.url,
+                "repository_url": issue.repository.url,
+                "labels_url": issue.labels_url,
+                "comments_url": issue.comments_url,
+                "events_url": issue.events_url,
+                "html_url": issue.html_url,
+                "number": issue.number,
+                "state": issue.state,
+                "title": issue.title,
+                "body": issue.body,
+                "user": self.get_user_info(
+                    issue.user),
+                "assignee": None if issue.assignee is None else self.get_user_info(
+                    issue.assignee),
+                "milestone": None if issue.milestone is None else self.milestone(
+                    issue.milestone),
+                "labels": None if issue.labels is None else self.get_labels(
+                    issue.labels),
+                "comments": issue.comments,
+                "pull_request": None if issue.pull_request is None else self.get_pull_request(
+                    issue.pull_request),
+                "closed_at": self.get_isoformat_date(
+                    issue.closed_at),
+                "created_at": self.get_isoformat_date(
+                    issue.created_at),
+                "updated_at": self.get_isoformat_date(
+                    issue.updated_at)}
             issues.append(dic_issue)
         return issues
 
@@ -83,6 +90,7 @@ class GithubUser:
                             "html_url": issue.html_url,
                             "patch_url": issue.patch_url
                             }
+        return dic_pull_request
 
     def get_labels(self, labels):
         labels = []
@@ -115,22 +123,26 @@ class GithubUser:
         return dic_user_info
 
     def get_milestone(self, milestone):
-        milestone_dic = {"url": milestone.url,
-                         "hmtl_url": milestone.html_url,
-                         "labels_url": milestone.labels_url,
-                         "id": milestone.id,
-                         "number": milestone.number,
-                         "state": milestone.state,
-                         "title": milestone.title,
-                         "description": milestone.description,
-                         "creator": self.get_user_info(milestone.creator),
-                         "open_issues": milestone.open_issues,
-                         "closed_issues": milestone.closed_issues,
-                         "created_at": milestone.created_at,
-                         "updated_at": self.get_isoformat_date(milestone.updated_at),
-                         "closed_at": self.get_isoformat_date(milestone.closed_at),
-                         "due_on": self.get_isoformat_date(milesonte.due_on)
-                         }
+        milestone_dic = {
+            "url": milestone.url,
+            "hmtl_url": milestone.html_url,
+            "labels_url": milestone.labels_url,
+            "id": milestone.id,
+            "number": milestone.number,
+            "state": milestone.state,
+            "title": milestone.title,
+            "description": milestone.description,
+            "creator": self.get_user_info(
+                milestone.creator),
+            "open_issues": milestone.open_issues,
+            "closed_issues": milestone.closed_issues,
+            "created_at": milestone.created_at,
+            "updated_at": self.get_isoformat_date(
+                milestone.updated_at),
+            "closed_at": self.get_isoformat_date(
+                milestone.closed_at),
+            "due_on": self.get_isoformat_date(
+                milestone.due_on)}
         return milestone_dic
 
     def get_isoformat_date(self, date):
