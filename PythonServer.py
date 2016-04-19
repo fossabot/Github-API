3b558ab8535928d8da088d00748d4f39c757fa3b
marcelbernic <marcel.bernic.1@ulaval.ca>
2016-04-19 14:47:02 -0400

    Adde more routes and filtered JSONs

--- PythonServer.py
+++ PythonServer.py
@@ -15,6 +15,21 @@ g = Github(token)
 user = GithubUser.GithubUser(g)
 ret_JSON = json.dumps(user.get_projects())
 
+################################################
+# Filtering outuput                            #
+################################################
+
+maxMin = sorted(user.projects, key = lambda projects: projects.get('open_issues_count'), reverse=True)
+ret_JSON2 = json.dumps(maxMin)
+
+minMax = sorted(user.projects, key = lambda projects: projects.get('open_issues_count'))
+ret_JSON3 = json.dumps(minMax)
+
+contributors = sorted(user.projects, key = lambda projects: len(projects.get('contributors')), reverse=True)
+ret_JSON4 = json.dumps(contributors)
+
+################################################
+
 
 @app.route('/')
 def get_repos_name():
@@ -28,6 +43,32 @@ def getProject():
                         mimetype="application/json")
 
     return response
+
+
+@app.route('/filter_max_min', methods=['GET'])
+def getProject2():
+    response2 = Response(response=ret_JSON2,
+                        status=200,
+                        mimetype="application/json")
+
+    return response2
+
+@app.route('/filter_min_max', methods=['GET'])
+def getProject3():
+    response3 = Response(response=ret_JSON3,
+                        status=200,
+                        mimetype="application/json")
+
+    return response3
+
+@app.route('/filter_contributors', methods=['GET'])
+def getProject4():
+    response4 = Response(response=ret_JSON4,
+                        status=200,
+                        mimetype="application/json")
+
+    return response4
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
