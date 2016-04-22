bd54c7421705b5060b88531f9f4f6fe7cacb4709
Luis E. Obando <luiseduardo.obando@gmail.com>
2016-04-22 15:08:43 -0400

    Fix code check

--- tests/dummyTest.py
+++ tests/dummyTest.py
@@ -2,7 +2,7 @@
 
 import unittest
 from team9 import GithubUser
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
 
 class GithubUserTest(unittest.TestCase):
@@ -57,10 +57,6 @@ class GithubUserTest(unittest.TestCase):
     def test_givenAGithubUserWhenAskedToGetHisProjectsThenAllOfThemShouldBeReturned(self):
         projects = self.GithubUser.get_projects()
         self.assertEqual(len(projects), 1)
-
-    def test_givenAGithubUserWhenAskedToGetHisProjectsThenProjectIdShouldBeCorrect(self):
-        projects = self.GithubUser.get_projects()
-        self.assertEqual(projects[0].get("id"), 1)
 
     def test_givenAGithubUserWhenAskedToGetHisProjectsThenProjectIdShouldBeCorrect(self):
         projects = self.GithubUser.get_projects()
